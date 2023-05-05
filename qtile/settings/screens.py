0ea32992de43d3b04072ac437eeb0683a44fc19b
Frank Diaz <francito_03@hotmail.com>
2023-05-05 18:01:35 -0500

    Refactoring Qtile code

--- qtile/settings/screens.py
+++ qtile/settings/screens.py
@@ -5,6 +5,7 @@ from libqtile.log_utils import logger
 from .widgets import primary_widgets, secondary_widgets
 import subprocess
 
+
 def status_bar(widgets):
     return bar.Bar(widgets, 24, opacity=0.92)
 
@@ -30,12 +31,14 @@ else:
     connected_monitors = int(command.stdout.decode("UTF-8"))
 
 if connected_monitors > 1:
-    os.system("xrandr --output eDP-1 --primary --mode 1366x768 --pos \
+    os.system(
+        "xrandr --output eDP-1 --primary --mode 1366x768 --pos \
               0x456 --rotate normal --output HDMI-1 --mode 1920x1080 \
               --pos 1366x300 --rotate normal --output DVI-I-1-1 --mode \
-              1680x1050 --pos 3286x0 --rotate left")
+              1680x1050 --pos 3286x0 --rotate left"
+    )
 
-    keyboard_layout = "setxkbmap -layout us -variant intl" 
+    keyboard_layout = "setxkbmap -layout us -variant intl"
     for _ in range(1, connected_monitors):
         screens.append(Screen(top=status_bar(secondary_widgets())))
 
